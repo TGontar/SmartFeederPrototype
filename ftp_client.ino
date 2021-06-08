ad067dbf5567a2c0c904a31f62a27fca1d5d8afb
TGontar <gontar.ts@mail.ru>
2021-06-08 06:29:56 +0300

    working with sd card

--- ftp_client.ino
+++ ftp_client.ino
@@ -2,6 +2,10 @@
 
 #include "ftp_client.hpp"
 #include "ftp_credentials.hpp"
+#include <SPI.h>
+#include <SD.h>
+#define SD_CS_PIN  A6    // Подключение SD Card      
+  int i = 0;
 
 namespace esp8266_arduino {
 namespace ftp {
@@ -15,13 +19,15 @@ client test_client (
 }
 
 void setup() {
+
+  
   // Begin serial communication
   Serial.begin(115200);
   Serial.println(F("FTP Client Project"));
 
   // Connect to Wifi
-  String ssid = "Da";
-  String password = "12345678";
+  String ssid = "Students";
+  String password = "C12H22O11";
   WiFi.begin(ssid, password);
   Serial.print(F("Connecting"));
   while (WiFi.status() != WL_CONNECTED) {
@@ -32,31 +38,54 @@ void setup() {
   Serial.print(F("\n"));
   Serial.println(F("Connected!"));
 
-  // Create a test file
-  Serial.println(F("Creating test file."));
-  SPIFFS.begin();
-  fs::File file_handler;
-  file_handler = SPIFFS.open("/test.txt", "w");
-  file_handler.println(F("This is a test file."));
-  file_handler.close();
+
+
+
 
 }
 
-void hold_until_serial_input() {
-  while (Serial.available() <= 0) yield(); // Wait for Serial input
-  while (Serial.read() > 0) yield(); // Discard Serial input
-}
+
 
 void loop() {
-  Serial.println(F("Press enter to upload a test file to FTP server"));
-  hold_until_serial_input();
+  i += 10;
+   
 
-  esp8266_arduino::ftp::test_client.upload_file("/test.txt", "/public_html/test.txt");
 
+  // Create a test file
+  Serial.println(F("Creating test file."));
+  SD.begin(SD_CS_PIN);
+  File logFile = SD.open("log.txt", "w+");
+  String a = String("This is a test file updated. ") + String(i)+ String(" seconds passed");
+  logFile.println(a); // Записываем в файл строку data.
+  logFile.close();       // Закрываем файл.
+
+  String finalString = "";
+  File logFile1 = SD.open("log.txt");
+        // считываем все байты из файла и выводим их в COM-порт
+        while (logFile1.available()) {
+            finalString += (char)logFile1.read();
+        }
+        // закрываем файл
+        logFile1.close();
+  Serial.println(finalString);
+  SPIFFS.begin();
+  fs::File file_handler;
+  file_handler = SPIFFS.open("/log1.txt", "w+");
+  file_handler.println(finalString);
+  file_handler.close();
+
+
+  
+
+  esp8266_arduino::ftp::test_client.upload_file("/log1.txt", "/public_html/log1.txt");
+  
+    
   Serial.print(F("Heap: "));
   Serial.println(ESP.getFreeHeap());
 
   Serial.print(F("Heap Fragmentation: "));
   Serial.print(ESP.getHeapFragmentation());
   Serial.println("%");
+  i = int(i);
+  delay(10000);
 }
